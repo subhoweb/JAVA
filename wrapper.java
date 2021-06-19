4b5fd0b915d6b1bf079d228d715540d89095092d
Subhojyoti Majumder <64296932+subhoweb@users.noreply.github.com>
2021-06-19 14:56:48 +0530

    Add files via upload

---
+++ wrapper.java
@@ -0,0 +1,17 @@
+public class wrapper {
+	public static void main(String[] args) {
+		    Integer myInt = 5; //Wrapper Objects
+		    Double myDouble = 5.99; //Wrapper Objects
+		    Character myChar = 'A'; //Wrapper Objects
+		    
+		    System.out.println(myInt);
+		    System.out.println(myDouble.doubleValue()); //Wrapper Method
+		    System.out.println(myChar);
+		    
+		    Integer num = 10000;
+		    String svar = num.toString(); //String Conversion
+		    System.out.println(svar.length()); // Length Calculation
+
+	}
+
+}
